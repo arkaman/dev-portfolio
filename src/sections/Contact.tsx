3a0b1e1fa5b74104384de2820e41d51f612f0dca
arkaman <arkayanmanna@outlook.com>
2026-07-08 21:36:35 +0530

    refactor: simplify mouse event handlers in CardContainer and update import statement in Contact section

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -1,5 +1,5 @@
 import ContactInfo from "@/components/contact-info";
-import ContactForm from "@/components/contact-form";
+// import ContactForm from "@/components/contact-form";
 
 export default function Contact() {
     return (
